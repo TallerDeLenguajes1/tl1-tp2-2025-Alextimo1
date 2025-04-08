e247669966b6031fe3d8d24fb3901c7e81777dc2
Alejandro <alextimo2003@gmail.com>
2025-04-08 15:11:14 -0300

    Inicial 2

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
-int main () {
+#include <stdlib.h>
+#include <time.h>
+
+#define N 20
+
+int main() {
+    int i;
+    double vt[N];
+
+    srand(time(NULL));  // Inicializa semilla para números aleatorios
+
+    for(i = 0; i < N; i++) {
+        vt[i] = 1 + rand() % 100;
+        printf("%f   ", vt[i]);
+    }
 
     return 0;
 }
