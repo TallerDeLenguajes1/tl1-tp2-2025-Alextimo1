0690b9627b3e64e1b650128c973f1d24f429ac2a
Alejandro <alextimo2003@gmail.com>
2025-04-08 15:35:24 -0300

    Desarrollo de la funcion principal main

--- tp2_4.c
+++ tp2_4.c
@@ -4,7 +4,8 @@
 
 #define CANT_PC 5
 
-struct compu {
+struct compu
+{
     int velocidad;
     int anio;
     int cantidad_nucleos;
@@ -15,14 +16,35 @@ void listarPCs(struct compu pcs[], int cantidad);
 void mostrarMasVieja(struct compu pcs[], int cantidad);
 void mostrarMasVeloz(struct compu pcs[], int cantidad);
 
-int main () {
+int main()
+{
 
+    struct compu pcs[CANT_PC];
+    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+
+    srand(time(NULL));
+
+    for (int i = 0; i < CANT_PC; i++)
+    {
+        pcs[i].velocidad = 1 + rand() % 3;
+        pcs[i].anio = 2015 + rand() % 10;
+        pcs[i].cantidad_nucleos = 1 + rand() % 8;
+        pcs[i].tipo_cpu = tipos[rand() % 6];
+    }
+
+    listarPCs(pcs, CANT_PC);
+    printf("\n----- PC más vieja -----\n");
+    mostrarMasVieja(pcs, CANT_PC);
+    printf("\n----- PC más veloz -----\n");
+    mostrarMasVeloz(pcs, CANT_PC);
     return 0;
 }
 
-void listarPCs(struct compu pcs[], int cantidad) {
-    for(int i = 0; i < cantidad; i++) {
-        printf("PC %d:\n", i+1);
+void listarPCs(struct compu pcs[], int cantidad)
+{
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("PC %d:\n", i + 1);
         printf("  Velocidad: %d GHz\n", pcs[i].velocidad);
         printf("  Año: %d\n", pcs[i].anio);
         printf("  Núcleos: %d\n", pcs[i].cantidad_nucleos);
@@ -31,11 +53,14 @@ void listarPCs(struct compu pcs[], int cantidad) {
     }
 }
 
-void mostrarMasVieja(struct compu pcs[], int cantidad) {
+void mostrarMasVieja(struct compu pcs[], int cantidad)
+{
     int min = pcs[0].anio;
     int indice = 0;
-    for(int i = 1; i < cantidad; i++) {
-        if(pcs[i].anio < min) {
+    for (int i = 1; i < cantidad; i++)
+    {
+        if (pcs[i].anio < min)
+        {
             min = pcs[i].anio;
             indice = i;
         }
@@ -43,11 +68,14 @@ void mostrarMasVieja(struct compu pcs[], int cantidad) {
     listarPCs(&pcs[indice], 1);
 }
 
-void mostrarMasVeloz(struct compu pcs[], int cantidad) {
+void mostrarMasVeloz(struct compu pcs[], int cantidad)
+{
     int max = pcs[0].velocidad;
     int indice = 0;
-    for(int i = 1; i < cantidad; i++) {
-        if(pcs[i].velocidad > max) {
+    for (int i = 1; i < cantidad; i++)
+    {
+        if (pcs[i].velocidad > max)
+        {
             max = pcs[i].velocidad;
             indice = i;
         }
