1b85a85bfbf7f96fd2eda421d1dd2e5ae57136e7
Alejandro <alextimo2003@gmail.com>
2025-04-08 15:32:32 -0300

    Desarrollo de la funcion listarPCs

--- tp2_4.c
+++ tp2_4.c
@@ -19,3 +19,14 @@ int main () {
 
     return 0;
 }
+
+void listarPCs(struct compu pcs[], int cantidad) {
+    for(int i = 0; i < cantidad; i++) {
+        printf("PC %d:\n", i+1);
+        printf("  Velocidad: %d GHz\n", pcs[i].velocidad);
+        printf("  Año: %d\n", pcs[i].anio);
+        printf("  Núcleos: %d\n", pcs[i].cantidad_nucleos);
+        printf("  Tipo CPU: %s\n", pcs[i].tipo_cpu);
+        printf("------------------------\n");
+    }
+}
